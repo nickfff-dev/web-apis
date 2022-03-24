7e47a34b6c17f0758e8e7abde3d6dfc7442d7db9
nickfff-dev <hunchographics@gmail.com>
2022-03-24 18:53:03 +0300

    c

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ sensor.onerror = event => console.log(event.error.name, event.error.message);
 sensor.onreading = () => {
   sensor.populateMatrix(mat4);
 };
+
+
+let startElem = document.querySelector("p");
+let endElem = startElem.querySelector("span");
+let range = document.createRange();
+
+range.setStart(startElem, 0);
+range.setEnd(endElem, endElem.childNodes[0].length/2);
+let contents = range.cloneContents();
+
+document.body.appendChild(contents);
